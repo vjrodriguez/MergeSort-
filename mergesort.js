55887cee17b6fc45e357b4e9b859a633cd9981e9
Vicky Rodriguez <vrod8763@gmail.com>
2020-05-27 17:07:33 -0400

    update mergeSort

--- mergesort.js
+++ mergesort.js
@@ -1,6 +1,44 @@
 
 function split(wholeArray) {
+  if(wholeArray.length <= 1) {
+    return wholeArray
+  }
   const firstHalf = wholeArray.slice(0, Math.floor(wholeArray.length/2))
   const secondHalf = wholeArray.slice(Math.floor(wholeArray.length/2))
   return [firstHalf, secondHalf];
 }
+//[3,4,5]
+//[1,8]
+//[1,3,4,5,8]
+
+function merge(arr1, arr2) {
+  //debugger;
+  if(!arr1.length || !arr2.length) {
+    return arr1.concat(arr2)
+  } else {
+    const arrToPop = arr1[0] > arr2[0] ? arr2 : arr1
+    const merged = [arrToPop.shift()].concat(merge(arr1, arr2))
+    return merged
+  }
+}
+
+//[1,4,5,6]
+//[[1,4], [5,6]]
+//so we would call recursion on the elements but not the entire array?
+function mergeSort(array) {
+  if(array.length < 2) {
+    return array
+  }
+  const splitted = split(array)
+  const firstHalf = splitted[0]
+  const secondHalf = splitted[1]
+  const sortedFirst = mergeSort(firstHalf)
+  const sortedSecond = mergeSort(secondHalf)
+  return merge(sortedFirst, sortedSecond)
+}
+// const splitted = split(array)
+// const leftSide = splitted[0] //[1,4]
+// const rightside = splitted[1]  //[5,6]
+//call recursion on leftside and right side, call recursion on just one and push back the returned array.
+//call mergeSort on split array
+//bring back merge after calling recursion
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,20 +1,45 @@
 describe('Split Array function', function() {
   it('is able to split arrays with one value', function() {
-    expect(split([1]).toEqual([1]))
+    expect(split([1])).toEqual([1])
   });
   it('does not split an empty array', function() {
-    expect(split([]).toEqual([]))
+    expect(split([])).toEqual([])
   });
   it('split ararys with even number of elements', function() {
-    expect(split([1,2,3,4,5,6]).toEqual([[1,2,3],[4,5,6]]))
+    expect(split([1,2,3,4,5,6])).toEqual([[1,2,3],[4,5,6]])
   });
   it('is able to split arrays with odd number of elements', function() {
-    expect(split([1,2,3,4,5,6,7]).toEqual([[1,2,3],[4,5,6,7]]))
+    expect(split([1,2,3,4,5,6,7])).toEqual([[1,2,3],[4,5,6,7]])
   });
 });
 
 describe('Merge function', function(){
   it('is able to merge two sorted arrays into one sorted array', function(){
-    // test the merging algorithm
+    expect(merge([1,2],[3,4])).toEqual([1,2,3,4])
+    expect(merge([1,3],[2,4])).toEqual([1,2,3,4])
   });
+  it('is able to merge two sorted arrays of different length', function(){
+    expect(merge([1,2,3],[4])).toEqual([1,2,3,4])
+  })
+  it('is able to merge two empty arrays', function(){
+    expect(merge([],[])).toEqual([])
+  })
 });
+
+describe('merge sort function', function(){
+  it('merges and sorts two arrays each with a single element', function(){
+    expect(mergeSort([1,2])).toEqual([1,2])
+  });
+  it('merges and sorts two arrays of the same length', function(){
+    expect(mergeSort([7,4,3,8])).toEqual([3,4,7,8])
+  });
+  it('merges and sorts two arrays of different length', function(){
+    expect(mergeSort([3,2,5,9,8,4,6])).toEqual([2,3,4,5,6,8,9])
+  });
+  it('merges and sorts two empty arrays', function(){
+    expect(mergeSort([])).toEqual([])
+  });
+  it('merges and sorts an empty array and a not empty array', function(){
+    expect(mergeSort([1000])).toEqual([1000])
+  });
+})
